d52ebe6796db9f2332378fd341643dda8df62bb6
Robert Gieseke <rob.g@web.de>
2024-03-27 13:18:11 +0100

    Add Global Carbon Budget 2023 and 2022

--- src/openclimatedata/__init__.py
+++ src/openclimatedata/__init__.py
@@ -1,13 +1,13 @@
 from ._ceds import CEDS
 from ._primap_hist import PRIMAPHIST
-from ._gcb import GCB
+from ._global_carbon_budget import GCB_Global_Carbon_Budget
 from ._gcb_national import GCB_National_Emissions
 from ._gcb_fossil import GCB_Fossil_Emissions
 
 __all__ = [
     "PRIMAPHIST",
     "CEDS",
-    "GCB",
+    "GCB_Global_Carbon_Budget",
     "GCB_National_Emissions",
     "GCB_Fossil_Emissions",
 ]
