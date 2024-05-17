619407e7b3b9aa538ca6be9d525aa1c65efdb18e
Robert Gieseke <rob.g@web.de>
2024-05-17 16:26:35 +0200

    Refactor PRIMAP-hist structure to be nested for files

--- tests/test_primaphist.py
+++ tests/test_primaphist.py
@@ -19,13 +19,13 @@ def test_primaphist():
 
 @pytest.mark.skipif(GITHUB_ACTIONS, reason="Test requires downloading.")
 def test_primaphist_2_5_1():
-    df = PRIMAPHIST["2.5.1"].to_dataframe()
+    df = PRIMAPHIST["2.5.1"]["main"].to_dataframe()
     assert df.iloc[0]["1750"] == approx(0.00564)
     assert df.iloc[-1]["2022"] == approx(0.0)
 
     assert df.iloc[0]["provenance"] == "derived"
 
-    ocdf = PRIMAPHIST["2.5.1"].to_ocd()
+    ocdf = PRIMAPHIST["2.5.1"]["main"].to_ocd()
     # First and last value should be the same after re-shaping.
     assert ocdf.iloc[0]["value"] == df.iloc[0]["1750"]
     assert ocdf.iloc[-1]["value"] == df.iloc[-1]["2022"]
@@ -35,13 +35,13 @@ def test_primaphist_2_5_1():
 
 @pytest.mark.skipif(GITHUB_ACTIONS, reason="Test requires downloading.")
 def test_primaphist_2_5():
-    df = PRIMAPHIST["2.5"].to_dataframe()
+    df = PRIMAPHIST["2.5"]["main"].to_dataframe()
     assert df.iloc[0]["1750"] == approx(0.00564)
     assert df.iloc[-1]["2022"] == approx(0.0)
 
     assert df.iloc[0]["provenance"] == "measured"
 
-    ocdf = PRIMAPHIST["2.5"].to_ocd()
+    ocdf = PRIMAPHIST["2.5"]["main"].to_ocd()
     # First and last value should be the same after re-shaping.
     assert ocdf.iloc[0]["value"] == df.iloc[0]["1750"]
     assert ocdf.iloc[-1]["value"] == df.iloc[-1]["2022"]
@@ -51,11 +51,11 @@ def test_primaphist_2_5():
 
 @pytest.mark.skipif(GITHUB_ACTIONS, reason="Test requires downloading.")
 def test_primaphist_2_4_2():
-    df = PRIMAPHIST["2.4.2"].to_dataframe()
+    df = PRIMAPHIST["2.4.2"]["main"].to_dataframe()
     assert df.iloc[0]["1750"] == approx(0.00564)
     assert df.iloc[-1]["2021"] == approx(13.9)
 
-    ocdf = PRIMAPHIST["2.4.2"].to_ocd()
+    ocdf = PRIMAPHIST["2.4.2"]["main"].to_ocd()
     # First and last value should be the same after re-shaping.
     assert ocdf.iloc[0]["value"] == df.iloc[0]["1750"]
     assert ocdf.iloc[-1]["value"] == df.iloc[-1]["2021"]
@@ -65,11 +65,11 @@ def test_primaphist_2_4_2():
 
 @pytest.mark.skipif(GITHUB_ACTIONS, reason="Test requires downloading.")
 def test_primaphist_2_3():
-    df = PRIMAPHIST["2.3"].to_dataframe()
+    df = PRIMAPHIST["2.3"]["main"].to_dataframe()
     assert df.iloc[0]["1750"] == approx(0.00564)
     assert df.iloc[-1]["2019"] == approx(15.6)
 
-    ocdf = PRIMAPHIST["2.3"].to_ocd()
+    ocdf = PRIMAPHIST["2.3"]["main"].to_ocd()
     # First and last value should be the same after re-shaping.
     assert ocdf.iloc[0]["value"] == df.iloc[0]["1750"]
     assert ocdf.iloc[-1]["value"] == df.iloc[-1]["2019"]
@@ -79,11 +79,11 @@ def test_primaphist_2_3():
 
 @pytest.mark.skipif(GITHUB_ACTIONS, reason="Test requires downloading.")
 def test_primaphist_2_2():
-    df = PRIMAPHIST["2.2"].to_dataframe()
+    df = PRIMAPHIST["2.2"]["main"].to_dataframe()
     assert df.iloc[0]["1850"] == approx(1.53e-04)
     assert df.iloc[-1]["2018"] == approx(1.99e-02)
 
-    ocdf = PRIMAPHIST["2.2"].to_ocd()
+    ocdf = PRIMAPHIST["2.2"]["main"].to_ocd()
     # First and last value should be the same after re-shaping.
     assert ocdf.iloc[0]["value"] == df.iloc[0]["1850"]
     assert ocdf.iloc[-1]["value"] == df.iloc[-1]["2018"]
