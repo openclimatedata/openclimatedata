54aa579bf2c768a6493d02070a4de5ebfb34ebf7
Robert Gieseke <rob.g@web.de>
2026-03-06 10:42:56 +0100

    Add PRIMAP-hist downloads

--- scripts/generate-webpage.py
+++ scripts/generate-webpage.py
@@ -11,30 +11,46 @@
 from pathlib import Path
 
 import openclimatedata as ocd
+from tqdm import tqdm
 
 root = Path(__file__).parents[1]
 
-html = """<!doctype html>
+html = f"""<!doctype html>
 <html lang=en>
 <head>
 <meta charset=utf-8>
+<meta name="robots" content="noindex">
 <title>Openclimatedata - Downloads</title>
 </head>
 <body>
-<ul>
-<h1>Openclimatedata Downloads</h1>
+<h1>Openclimatedata Downloads - Version {ocd.__version__}</h1>
 <p><a href="https://github.com/openclimatedata/openclimatedata">GitHub Repo</a></p>
 """
 
 gcb_versions = ocd.GCB_Fossil_Emissions.keys()
 
-html += """<h2>GCB Fossil</h2>"""
+html += """<h2>GCB Fossil</h2>
+<ul>"""
 
-for gcb_version in gcb_versions:
+print("GCB Fossil")
+for gcb_version in tqdm(gcb_versions):
     df = ocd.GCB_Fossil_Emissions[gcb_version].to_ocd()
     filename = f"gcb-fossil-{gcb_version}.parquet"
     df.to_parquet(root / "scripts" / filename)
-    html += f'<li><a href="{filename}">{filename}</a></li>\n'
+    html += f'<li><a href="{filename}">{filename}</a> ({ocd.GCB_Fossil_Emissions[gcb_version].license})</li>\n'
+
+primaphist_versions = ocd.PRIMAPHIST.keys()
+
+html += """</ul>
+<h2>PRIMAP-hist</h2>
+<ul>"""
+
+print("PRIMAP-hist")
+for primaphist_version in tqdm(primaphist_versions):
+    df = ocd.PRIMAPHIST[primaphist_version]["main"].to_ocd()
+    filename = f"primap-hist-{primaphist_version.replace('.', '-')}.parquet"
+    df.to_parquet(root / "scripts" / filename)
+    html += f'<li><a href="{filename}">{filename}</a> ({ocd.PRIMAPHIST[primaphist_version].license})</li>\n'
 
 html += """</ul>
 </body>
