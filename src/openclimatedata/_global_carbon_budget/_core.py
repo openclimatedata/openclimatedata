bbef5cb05f6a6b82d4b65a86bb29b94b575b767a
Robert Gieseke <rob.g@web.de>
2026-04-15 14:40:18 +0200

    Fix column name mangling
    Closes #38

--- src/openclimatedata/_global_carbon_budget/_core.py
+++ src/openclimatedata/_global_carbon_budget/_core.py
@@ -153,9 +153,10 @@ class _Global_Carbon_Budget_Table:
             usecols=self.columns,
             index_col=0,
         )
-        # Remove suffixes from duplicated columns names
+        # Remove suffixes `.1`, `.2`, etc. from duplicated columns names
         # (added by Pandas, see https://github.com/pandas-dev/pandas/issues/64198).
         # `CLM5.0`` needs to remain
+        df.columns = df.columns.map(lambda x: re.sub(r"\.[123]$", "", x))
         df = df.rename(columns={"LPX-Bern.1": "LPX-Bern"})
         df.columns = df.columns.str.strip()
         df.name = self.table_name
