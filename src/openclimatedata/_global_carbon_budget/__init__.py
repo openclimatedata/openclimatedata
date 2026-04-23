8469042e33682e26b9929b625d6cb74d4db21242
Robert Gieseke <rob.g@web.de>
2026-04-23 13:47:21 +0200

    Add GCB 2019

--- src/openclimatedata/_global_carbon_budget/__init__.py
+++ src/openclimatedata/_global_carbon_budget/__init__.py
@@ -1,3 +1,4 @@
+from ._gcb2019 import GCB2019
 from ._gcb2021 import GCB2021
 from ._gcb2022 import GCB2022
 from ._gcb2023 import GCB2023
@@ -8,4 +9,5 @@ Global_Carbon_Budget = {
     "2023": GCB2023,
     "2022": GCB2022,
     "2021": GCB2021,
+    "2019": GCB2019,
 }
