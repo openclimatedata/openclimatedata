dcf9d1cc38c23aa34dbde509f985ee156ccf0a70
Robert Gieseke <rob.g@web.de>
2026-05-27 14:54:34 +0200

    Refactor GCB core to include National emissions Excel files

--- src/openclimatedata/__init__.py
+++ src/openclimatedata/__init__.py
@@ -1,6 +1,5 @@
 from ._ceds import CEDS
 from ._gcb_fossil import GCB_Fossil_Emissions
-from ._gcb_national import GCB_National_Emissions
 from ._global_carbon_budget import Global_Carbon_Budget
 from ._primap_hist import PRIMAPHIST
 
@@ -8,7 +7,6 @@ __all__ = [
     "PRIMAPHIST",
     "CEDS",
     "Global_Carbon_Budget",
-    "GCB_National_Emissions",
     "GCB_Fossil_Emissions",
 ]
 
